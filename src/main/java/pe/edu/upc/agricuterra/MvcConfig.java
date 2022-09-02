5b0d79d75a499e491819105aa29cfae4a660a6f5
JorgeDCh20 <JorgeDCh20>
2022-09-01 21:41:38 -0500

    version 1

---
+++ src/main/java/pe/edu/upc/agricuterra/MvcConfig.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.agricuterra;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error").setViewName("error");
+		
+	}
+
+}
